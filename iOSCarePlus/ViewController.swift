f3052d820893bb40963d9fc186fc2eb98dbb2f6d
p41155a <50395024+p41155a@users.noreply.github.com>
2020-12-11 11:20:39 +0900

    [👩🏻‍💻메인] 메인화면 작업 및 swiftLint 작업

--- iOSCarePlus/ViewController.swift
+++ iOSCarePlus/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var logoView: UIView!
+    @IBOutlet private weak var logoViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var backgroundImageViewLeadingConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logoView.layer.cornerRadius = 15
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animationSettingDefault()
+        // 다른 탭에 갔다왔을 때 애니메이션이 가능하도록
+        
+        apperLogoViewAnimation { [weak self] in
+            self?.slideBackgroundImageAnimation()
+            self?.blinkLogoAnimation()
+        }
+    }
+    
+    private func animationSettingDefault() {
+        logoViewTopConstraint.constant = -200
+        backgroundImageViewLeadingConstraint.constant = 0
+        logoView.alpha = 1
+        view.layoutIfNeeded()
+    }
+    
+    private func apperLogoViewAnimation(completion: @escaping () -> Void ) {
+        UIView.animate(withDuration: 0.7,
+                       delay: 1,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 1,
+                       options: []
+        ) { [weak self] in
+            self?.logoViewTopConstraint.constant = 17
+            self?.view.layoutIfNeeded()
+            // 화면을 갱신하라고 해야 변화하기 때문에 쓰는 코드
+        } completion: { _ in
+            completion()
+        }
+    }
+    
+    private func slideBackgroundImageAnimation() {
+        //curveEaseInOut : 빠르게 진입 후 빠르게 위치
+        UIView.animate(withDuration: 5, delay: 0, options: [.curveEaseInOut, .repeat, .autoreverse]) { [weak self] in
+            self?.backgroundImageViewLeadingConstraint.constant = -800
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    private func blinkLogoAnimation() {
+        UIView.animate(withDuration: 1, delay: 0, options: [.repeat, .autoreverse]) { [weak self] in
+            // 히든을 하면 안되는 이유: 1과 0으로 이루어져 있어서 서서히 애니메이션이 안됨
+            self?.logoView.alpha = 0
+        }
+    }
 }
 
