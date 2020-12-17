ae8a25641697f1d30932b34b8a39631eab91b6e1
p41155a <50395024+p41155a@users.noreply.github.com>
2020-12-17 12:02:33 +0900

    ✨NewGameResponse Model의 screenshots 값 추가

--- iOSCarePlus/NewGameResponse.swift
+++ iOSCarePlus/NewGameResponse.swift
@@ -7,22 +7,31 @@
 
 import Foundation
 
-struct NewGameContent: Decodable {
-    let formalName: String
-    let heroBannerURL: String
-    let gameId: Int
-//    let screenshots: []
-    
-    enum CodingKeys: String, CodingKey {
-        case formalName = "formal_name"
-        case heroBannerURL = "hero_banner_url"
-        case gameId = "id"
-    }
-}
-
 struct NewGameResponse: Decodable {
     let contents: [NewGameContent]
     let offset: Int
     let length: Int
     let total: Int
+}
+
+struct NewGameContent: Decodable {
+    let formalName: String
+    let heroBannerURL: String
+    let gameId: Int
+    let screenshots: [NewGameScreenshots]
+    
+    enum CodingKeys: String, CodingKey {
+        case formalName = "formal_name"
+        case heroBannerURL = "hero_banner_url"
+        case gameId = "id"
+        case screenshots
+    }
+}
+
+struct NewGameScreenshots: Decodable {
+    let images: [NewGameURL]
+}
+
+struct NewGameURL: Decodable {
+    let url: String
 }
